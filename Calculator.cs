da45a27aea3e2950bb8808774cdcccf133b8b337
Viacheslav Melnyk <altt1m.zaskar@gmail.com>
2023-12-19 22:46:31 +0200

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Hometask1
+{
+    public static class Calculator
+    {
+        public static double AddNumbers(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double SubNumbers(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double MulNumbers(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double DivNumbers(double num1, double num2)
+        {
+            if (num2 !=0 )
+            {
+                return num1 / num2;
+            }
+            else
+            {
+                throw new DivideByZeroException("Nizya na zero");
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Hometask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double num1, num2;
+                try
+                {
+                    Console.Write("Enter your first number: "); // first num
+                    num1 = int.Parse(Console.ReadLine());
+
+                    Console.Write("Enter your second number: "); // second num
+                    num2 = int.Parse(Console.ReadLine());
+
+                    Console.Clear(); // new line
+                }
+                catch
+                {
+                    Console.Clear();
+                    continue;
+                }
+                
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine($"Num1 = {num1} Num2 = {num2}");
+                        Console.WriteLine("\"1\" - add\n" +
+                                            "\"2\" - substract\n" +
+                                            "\"3\" - multiply\n" +
+                                            "\"4\" - divide\n" +
+                                            "or \"0\" to return.");
+                        Console.Write("Choose your operation: ");
+                        int response = int.Parse(Console.ReadLine());
+
+                        if (response == 0) 
+                        {
+                            Console.Clear();
+                            break; 
+                        }
+                        double res;
+                        switch (response)
+                        {
+                            case 1:
+                                res = Calculator.AddNumbers(num1, num2);
+                                Console.WriteLine($"{num1} + {num2} = {res}\n");
+                                
+                                break;
+                            case 2:
+                                res = Calculator.SubNumbers(num1, num2);
+                                Console.WriteLine($"{num1} - {num2} = {res}\n");
+                                break;
+                            case 3:
+                                res = Calculator.MulNumbers(num1, num2);
+                                Console.WriteLine($"{num1} * {num2} = {res}\n");
+                                break;
+                            case 4:
+                                res =Calculator.DivNumbers(num1, num2);
+                                Console.WriteLine($"{num1} / {num2} = {res}\n");
+                                break;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine(e.Message);
+                        
+                        continue;
+                    }
+                    
+                }
+                
+
+            }
+            
+        }
+    }
+}
